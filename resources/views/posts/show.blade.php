8412d8948ad58fb88960ed4f9cd79ce4594c63b5
mwangangimusyoka1998@gmail.com <mwangangimusyoka1998@gmail.com>
2023-04-09 00:20:53 +0300

    done

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,10 +6,10 @@
 
         <h3 class="text text-center">{{ $post->title }}</h3>
         <hr>
-        <img src="{{ url('/cover_image/'.$post->cover_image) }}" alt="image" style="width:50%;">
+        <img src="{{ url('/cover_image/' . $post->cover_image) }}" alt="image" style="width:50%;">
         <div>
             <p>{!! $post->body !!}</p>
-
+            <hr>
             <small>published on: {{ $post->created_at }} by {{ $post->user->name }}</small>
         </div>
         @if (!Auth::guest())
