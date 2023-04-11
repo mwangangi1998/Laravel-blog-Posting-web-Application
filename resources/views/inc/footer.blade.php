d9dd69e95bd5d340940fb8ed0118126e540e344d
mwangangimusyoka1998@gmail.com <mwangangimusyoka1998@gmail.com>
2023-04-11 10:17:47 +0300

    done ..

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,37 +1,22 @@
 <!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="container my-5">
+
 
     <section class="">
         <!-- Footer -->
         <footer class="text-center text-white" style="background-color: #0a4275;">
-            <!-- Grid container -->
-            <div class="container p-4 pb-0">
-                <!-- Section: CTA -->
-                <section class="">
-                    <p class="d-flex justify-content-center align-items-center">
-                        <span class="me-3 b-300">subscribe to our news letter</span>
-                        <a href="{{ url('inc.newsletter') }}">
-                            <button type="button" class="btn btn-outline-light btn-rounded" id="myButton">
-                                subscribe
-                            </button>
-                        </a>
-                    </p>
-                </section>
-                <!-- Section: CTA -->
-            </div>
-            <!-- Grid container -->
+
 
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            <b>
-                &copy; <span id="year"></span>
-            </b>
-                <a class="text-white text-uppercase" href="{{ url("/") }}">vtech.com</a>
+                <b>
+                    &copy; <span id="year"></span>
+                </b>
+                <a class="text-white text-uppercase" href="{{ url('/') }}">vtech.com</a>
             </div>
             <!-- Copyright -->
         </footer>
         <!-- Footer -->
-    </section>
+
 
 </div>
 <!-- End of .container -->
@@ -45,6 +30,8 @@
         width: 100%;
     }
 
+
+
     @media screen and (max-width: 768px) {
         footer {
             left: 0;
@@ -56,19 +43,5 @@
 <script>
 document.getElementById('year').innerHTML
 =new Date().getFullYear();
-
-
-// pop up form for subscription
-// JavaScript code
-var myButton = document.getElementById("myButton");
-var myPopupForm = document.getElementById("myPopupForm");
-
-myButton.addEventListener("click", function() {
-  if (myPopupForm.style.display === "none") {
-    myPopupForm.style.display = "block"; // Show the pop-up form
-  } else {
-    myPopupForm.style.display = "none"; // Hide the pop-up form
-  }
-});
 
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,16 +8,19 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-
+@vite(['resources/css/app.css'])
 </head>
 <body>
+    <div class="loader">
+
+    </div>
     <div id="app">
       @include('inc.navbar')
 <div class="container">
@@ -32,5 +35,17 @@
     </div>
     <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
     @vite(['resources/js/app.js'])
+
+    <script>
+        window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    // loader.classList.add("loader--hidden");
+loader.classList.add('loader--hidden');
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+    </script>
 </body>
 </html>
